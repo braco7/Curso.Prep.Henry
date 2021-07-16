774fefc5297584e40437691d0c491da194d64c0b
Fran <frajarsa@gmail.com>
2021-07-16 17:14:15 -0300

    Segunda parte de Javascript Henry Prep course

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -36,7 +36,7 @@ function conection(status) {
   } else if (status == 2) {
     return "Away"
   } else {
-    "Offline"
+    return "Offline"
   }
 }
 
@@ -133,9 +133,9 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if (num1 > num2 && num1 > num3) {
-    if (num1 > 0 && num2 > 0 && num3 > 0) {
-      return "Numero 1 es mayor y positivo"
+  if (num1 > num2 && num1 > num3 && num1 > 0) {
+    if (num2 > 0 && num3 > 0) {
+      return "Número 1 es mayor y positivo"
     } else if (num1 < 0 || num2 < 0 || num3 < 0) {
       return "Hay negativos"
     } else if (num1 == 0 || num2 == 0 || num3 == 0) {
@@ -161,18 +161,17 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if (numero === 1) {
+  if (numero < 2) {
     return false;
   } else if (numero === 2) {
     return true;
-  } else {
-    for (let i = 2; i < numero; i++) {
-      if (numero % i === 0) {
-        return false;
+  }
+  for (let i = 3; i < numero; i++) {
+    if (numero % i === 0) {
+      return false;
       }
     }
-    return true;
-  }
+  return true;
 }
 
 function esVerdadero(valor) {
@@ -209,14 +208,14 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-  variable = 5
-  i = 0
+  let i = 0
+  let num = numero
   do {
-    variable = variable +5
+    num = num +5
     i++
   }
-  while (1<=8)
-  return variable
+  while (i<8)
+  return num
 }
 
 
